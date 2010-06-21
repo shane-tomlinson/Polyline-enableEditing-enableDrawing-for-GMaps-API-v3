5f3669567dcc3bd519b562823865558a426d81c6
Rene Saarsoo <nene@triin.net>
2010-06-21 20:10:00 +0300

    Created simple build script in Bash.
    It concatenates all files marked with #include in test.html and
    places the result into build/ directory.
    
    Also added .gitignore to ignore the build/ dir.

---
+++ build.bash
@@ -0,0 +1,23 @@
+#!/usr/bin/env bash
+
+# Recreate the build directory
+rm -rf ./build
+mkdir ./build
+
+# concatenate files marked with <!-- #include --> into mapsextensions.js
+for filename in $(grep "#include" html/test.html | sed -r 's/.*src="...(.*)".*/\1/'); do
+    cat $filename >> ./build/mapsextensions.js
+done
+
+# copy over spherical_formulas.js
+cp javascripts/spherical_formulas.js ./build/spherical_formulas.js
+
+# Copy over test.html and correct <script> tags in it.
+#
+# Remove lines having <!-- #include -->
+# Replace <!-- #insert --> with link to mapsextensions.js
+# Correct the path to spherical_formulas.js
+grep -v "#include" html/test.html | \
+    sed 's/.*#insert.*/<script type="text\/javascript" src="mapsextensions.js"><\/script>/' | \
+    sed 's/..\/javascripts\/spherical_formulas.js/spherical_formulas.js/' > ./build/test.html
+
