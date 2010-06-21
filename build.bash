40111989eedefa4f4cc2209109454eeb7380b0bb
Rene Saarsoo <nene@triin.net>
2010-06-21 20:19:59 +0300

    Placed concatenated code inside anonymous function.
    __extend() and __bind() functions no more pollute the global scope.

--- build.bash
+++ build.bash
@@ -5,9 +5,13 @@ rm -rf ./build
 mkdir ./build
 
 # concatenate files marked with <!-- #include --> into mapsextensions.js
+# enclose the contents of the file inside anonymous function
+# to avoid polluting global namespace.
+echo "(function(){" >> ./build/mapsextensions.js
 for filename in $(grep "#include" html/test.html | sed -r 's/.*src="...(.*)".*/\1/'); do
     cat $filename >> ./build/mapsextensions.js
 done
+echo "})();" >> ./build/mapsextensions.js
 
 # copy over spherical_formulas.js
 cp javascripts/spherical_formulas.js ./build/spherical_formulas.js
